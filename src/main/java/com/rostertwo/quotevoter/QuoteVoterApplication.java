7c4fdee5de43203510e830f7ffb0ac39d3f93cfb
Aydar Sharafiev <aydarsh@gmail.com>
2023-02-10 09:27:22 +0300

    initial commit

---
+++ src/main/java/com/rostertwo/quotevoter/QuoteVoterApplication.java
@@ -0,0 +1,13 @@
+package com.rostertwo.quotevoter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuoteVoterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuoteVoterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rostertwo/quotevoter/QuoteVoterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rostertwo.quotevoter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuoteVoterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
