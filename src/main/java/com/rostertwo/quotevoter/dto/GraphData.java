db6f5d620066f737bd75e4f7c8585864b296712e
Aydar Sharafiev <aydarsh@gmail.com>
2023-02-13 20:19:57 +0300

    Added VoteController tests. closes #31

--- src/main/java/com/rostertwo/quotevoter/dto/GraphData.java
+++ src/main/java/com/rostertwo/quotevoter/dto/GraphData.java
@@ -1,12 +1,14 @@
 package com.rostertwo.quotevoter.dto;
 
 import com.rostertwo.quotevoter.enums.VoteType;
+import lombok.Builder;
 import lombok.Value;
 
 import java.time.LocalDate;
 import java.util.Map;
 
 @Value
+@Builder
 public class GraphData implements DataTransferObject {
 
     LocalDate date;
